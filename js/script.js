8362e3d0802984ecd54d6af833b8ee5ee10115dd
valerio <valerio.deprosperi@gmail.com>
2020-09-15 15:56:41 +0200

    oggetto studente

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*Creare un oggetto che descriva uno studente con le seguenti proprietà:
+nome, cognome e età. Stampare a schermo attraverso il for in tutte le
+proprietà.*/
+var studente = {
+    "nome": "Marco",
+    "cognome": "Olivetti",
+    "età": "28"
+}
